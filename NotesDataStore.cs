e83df567f36f76c06179f5edd96279a7a9cda8eb
Chase Fite <chasef7031@gmail.com>
2024-02-24 17:05:32 -0500

    Add note data store for temporary in memory data storage

---
+++ NotesDataStore.cs
@@ -0,0 +1,32 @@
+﻿using BaselineAPI.Models;
+
+namespace BaselineAPI
+{
+    /// <summary>
+    /// Temporary in memory data store.
+    /// </summary>
+    public class NotesDataStore
+    {
+        public List<NoteDto> Notes { get; set; }
+        public static NotesDataStore Current { get; } = new NotesDataStore();
+
+        public NotesDataStore() 
+        {
+            Notes = new List<NoteDto>()
+            {
+                new NoteDto()
+                {
+                    Id = 1,
+                    Title = "Title",
+                    Content = "Content"
+                },
+                new NoteDto()
+                {
+                    Id= 2,
+                    Title = "Title",
+                    Content = "Content"
+                }
+            };
+        }
+    }
+}
