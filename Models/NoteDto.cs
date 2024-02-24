f6de3fe3ba4cd05a82fa9e870df9fb019bf0e372
Chase Fite <chasef7031@gmail.com>
2024-02-24 17:05:00 -0500

    Add note dto class

---
+++ Models/NoteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BaselineAPI.Models
+{
+    public class NoteDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+    }
+}
