1b1de8ec7bb8f9a5b5ef6389d5a062100fce5a41
Chase Fite <chasef7031@gmail.com>
2024-02-24 16:29:35 -0500

    Setup initial get notes endpoint

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,6 +1,22 @@
-﻿namespace BaselineAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaselineAPI.Controllers
 {
-    public class NoteController
+    /// <summary>
+    /// The note controller class.
+    /// </summary>
+    [ApiController]
+    [Route("api/notes")]
+    public class NoteController : ControllerBase
     {
+        /// <summary>
+        /// Gets all note records. 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult GetNotes()
+        {
+            return new JsonResult(new { id = 1, title = "title", content = "content" });
+        }
     }
 }
