51963ccdab808724ea57232f0f93e4ab1506088d
Chase Fite <chasef7031@gmail.com>
2024-02-25 16:07:39 -0500

    Add dto class for note creation

---
+++ Models/NoteForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BaselineAPI.Models
+{
+    public class NoteForCreationDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content {  get; set; } = string.Empty;
+    }
+}
