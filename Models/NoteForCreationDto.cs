af2080e451804ffb56b3c973cb09286b4919cda6
Chase Fite <chasef7031@gmail.com>
2024-02-26 00:17:35 -0500

    Add data constraints for note creation

--- Models/NoteForCreationDto.cs
+++ Models/NoteForCreationDto.cs
@@ -1,8 +1,15 @@
-﻿namespace BaselineAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace BaselineAPI.Models
 {
     public class NoteForCreationDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; } = string.Empty;
+
+        [MaxLength(5000)]
         public string Content {  get; set; } = string.Empty;
     }
 }
