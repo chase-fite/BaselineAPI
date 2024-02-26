1724ebf5c9da365a6d0856f34e073d2434d94267
Chase Fite <chasef7031@gmail.com>
2024-02-26 04:12:10 -0500

    Add note class for update

---
+++ Models/NoteForUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaselineAPI.Models
+{
+    public class NoteForUpdateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [MaxLength(5000)]
+        public string Content {  get; set; } = string.Empty;
+    }
+}
